f05597919555b797dd6060a107dd706a25671a31
keith_liu <hikariyen881230@gmail.com>
2025-03-25 13:50:24 +0800

    2 log

--- src/html5-qrcode/index.tsx
+++ src/html5-qrcode/index.tsx
@@ -197,6 +197,7 @@ export const generateHtml5QrCode = (domId: string) => {
         camera.label.toLowerCase().includes("environment")
     );
     console.log({ cameras });
+    console.log({ backCamera });
 
     // 若找不到後鏡頭，則選擇第一個可用相機
     const selectedCameraId = backCamera ? backCamera.id : cameras[0].id;
